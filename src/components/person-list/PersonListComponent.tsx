46570deb08daa6aad0f30ade33d82cdc1148e890
Eric Kelly <github@useric.com>
2022-10-25 21:50:37 -0700

    Update iteration order

--- src/components/person-list/PersonListComponent.tsx
+++ src/components/person-list/PersonListComponent.tsx
@@ -8,22 +8,8 @@ interface PersonListComponentProps {
     setFirstPersonNameRef: (firstPersonName: TextInput | null) => void,
 }
 
-// Focus order: Name -> Contribution -> Name 2 -> Contribution 2 ...
-let inputList: Array<{ input: TextInput | null, elementId: string }> = [];
-const addToInputList = (elementId: string, input: TextInput | null) => {
-    inputList.push({ elementId, input });
-};
-function transitionToNextFocusElement(elementId: string) {
-    let indexOfElement = inputList.findIndex(value => elementId === value.elementId);
-    let indexOfNextElement = indexOfElement + 1;
-    if (indexOfNextElement < inputList.length) {
-        // Focus on the next element
-        let nextElement = inputList[indexOfNextElement].input;
-        if (nextElement) {
-            nextElement.focus();
-        }
-    }
-}
+let moneyInputList: Array<{ input: TextInput | null, elementId: string }> = [];
+let nameInputList: Array<{ input: TextInput | null, elementId: string }> = [];
 
 const PersonListComponent = ({ setFirstPersonNameRef }: PersonListComponentProps) => {
     const { state, actions: { addPerson }} = useContext(BillContext);
@@ -48,6 +34,32 @@ const PersonListComponent = ({ setFirstPersonNameRef }: PersonListComponentProps
         </TouchableOpacity>
     </View>;
 
+    function transitionToNextFocusElement(elementId: string) {
+        console.log(`Names: ${JSON.stringify(nameInputList.map(value => value.elementId))}`);
+        console.log(`Moneys: ${JSON.stringify(moneyInputList.map(value => value.elementId))}`);
+        let indexOfElement = nameInputList.findIndex(value => elementId === value.elementId);
+        console.log(`Found ${elementId} at index ${indexOfElement} / ${nameInputList.length - 1}`);
+        let arrayOfElements = nameInputList;
+        if (indexOfElement < 0) {
+            indexOfElement = moneyInputList.findIndex(value => elementId === value.elementId);
+            console.log(`Actually found ${elementId} at index ${indexOfElement} / ${moneyInputList.length - 1}`);
+            arrayOfElements = moneyInputList;            
+        } else if (indexOfElement == nameInputList.length - 1) {
+            console.log(`Switching lists`);
+            // Switch lists
+            indexOfElement = -1;
+            arrayOfElements = moneyInputList;
+        }
+        let indexOfNextElement = indexOfElement + 1;
+        
+        if (indexOfNextElement < arrayOfElements.length) {
+            // Focus on the next element
+            let nextElement = arrayOfElements[indexOfNextElement].input;
+            console.log(`Found ${nextElement}`);
+            nextElement?.focus();
+        }
+    }
+
     return (
         <View style={styles.container}>
             <Header />
@@ -57,11 +69,15 @@ const PersonListComponent = ({ setFirstPersonNameRef }: PersonListComponentProps
                         person={item} 
                         index={index} 
                         setNameInputRef={(nameInput, id) => {
-                            addToInputList(id, nameInput);
+                            if (!nameInputList.map(item => item.elementId).includes(id)) {
+                                nameInputList.push({ elementId: id, input: nameInput });
+                            }
                             if (index === 0) setFirstPersonNameRef(nameInput);
                         }}
                         setContributionInputRef={(contributionInput, id) => {
-                            addToInputList(id, contributionInput);
+                            if (!moneyInputList.map(item => item.elementId).includes(id)) {
+                                moneyInputList.push({ elementId: id, input: contributionInput });
+                            }
                         }}
                         onEndEditing={transitionToNextFocusElement}
                         />;
